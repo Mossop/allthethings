61733d8421d0875954508cb413244e973cc0deb9
Dave Townsend <dtownsend@oxymoronical.com>
2021-07-16 11:18:02 +0100

    Fix codegen.

--- plugins/github/server/codegen.js
+++ plugins/github/server/codegen.js
@@ -29,7 +29,7 @@ module.exports = {
         defaultScalarType: "string",
         useTypeImports: true,
         nameSuffix: "Query",
-        namingConvention: "camelCase",
+        namingConvention: "change-case-all#camelCase",
       },
       "add": {
         content: [
